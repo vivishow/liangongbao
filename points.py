ccbe8515ccb68e521a9b4ec0effb790c60cd2279
vivishow <cxy036@qq.com>
2024-06-29 11:34:11 +0800

    添加全局变量方便修改

--- points.py
+++ points.py
@@ -1,17 +1,42 @@
 import json
 from mitmproxy import http
 
+print("****************************************************************\n已加载")
+
+USERCODE = 15305433
+POINTS = 99
+DRAWNUM = 99
+
 
 def response(flow: http.HTTPFlow) -> None:
+    if "lgb360.com/aqy/" in flow.request.pretty_url:
+        request = {
+            "token": flow.request.headers["token"],
+            "id": flow.request.headers["memberid"],
+            "cookie": flow.request.headers["cookie"],
+        }
+        print(f"{request=}")
+        content = json.loads(flow.response.content.decode("UTF-8"))
+        print(f"{flow.request.pretty_url=}\n{content=}")
+        # if data := content.get("data"):
+        #     data["points"] = POINTS
+        # flow.response.content = json.dumps(content).encode("UTF-8")
+
     if "lgb360.com/aqy/regist/competition" in flow.request.pretty_url:
         content = json.loads(flow.response.content.decode("UTF-8"))
-        print(f"信息 {content}")
         if data := content.get("data"):
-            data['points'] = 999
+            data["points"] = POINTS
+            data["drawNum"] = DRAWNUM
         flow.response.content = json.dumps(content).encode("UTF-8")
 
     if "lgb360.com/aqy/regist/activity" in flow.request.pretty_url:
         content = json.loads(flow.response.content.decode("UTF-8"))
         if data := content.get("data"):
-            data["userCode"] = 7777777
+            data["userCode"] = USERCODE
+        flow.response.content = json.dumps(content).encode("UTF-8")
+
+    if "lgb360.com/aqy/prize/getDrawSurplusNum" in flow.request.pretty_url:
+        content = json.loads(flow.response.content.decode("UTF-8"))
+        if data := content.get("data"):
+            data["surplusNum"] = DRAWNUM
         flow.response.content = json.dumps(content).encode("UTF-8")
